0e7e048368cc6cb8613eaa5d2135e7cbc7a01f29
Ashwini Sahu <ashwinisahu990@gmail.com>
2021-08-08 19:23:17 +0530

    Printing the output in same line.
    *Fixes and other things :-*
    
    * Printing the output in same line.
    * The images will be saved in the naming convention of ***frame-<frame number>.png***
    * Linted the code.

--- videoToImage.py
+++ videoToImage.py
@@ -3,30 +3,34 @@ import os
 import cv2
 
 #give video path, replace it with your's video path
-video_path = 'video.mp4'
+VIDEO_PATH = "video.mp4"
+
 #start capturing the video
-capture = cv2.VideoCapture(video_path)
-img_count = 1
+capture = cv2.VideoCapture(VIDEO_PATH)
+
+#frame counter
+FRAME_COUNT = 1
+
 #create a directory to save the frames
 os.mkdir('VideoToImage')
 
 while capture.isOpened() :
     #store each frame in variable "frame"
     read_frame, frame = capture.read()
-    
-    if read_frame == True :
+
+    if read_frame is True :
         #save the image to the directory which was created
-        video_image = cv2.imwrite(f'VideoToImage//image{img_count}.png', frame)
-        
-        if video_image == True :
-            print(f'Image is saved at :- VideoToImage//image{img_count}.png')
-            img_count += 1
-            
+        video_image = cv2.imwrite(f'VideoToImage/frame-{FRAME_COUNT}.png', frame)
+
+        if video_image is True :
+            print(f'Image is saved at :- VideoToImage/frame-{FRAME_COUNT}.png', end="\r")
+            FRAME_COUNT += 1
+
         cv2.imshow('videoplayer', frame)
         #press 'q' to quite the operation
         if cv2.waitKey(25) & 0xff == ord('q') :
             break
-        
+
     else :
         print('Unable to read frame')
         break
